e18d6d4097ca9e960efd7af1436130feaf671693
KaranShah-Conestoga <133174025+KaranShah-Conestoga@users.noreply.github.com>
2023-12-06 13:04:22 -0500

    updare origin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ connectDB(DATABASE_URL)
 
 const io = new Server(server, {
     cors: {
-      origin: "event-blog-front-end-live-g2rxyw3zg-karan-shahs-projects.vercel.app",
+      origin: "event-blog-back-end-live.vercel.app",
       methods: ["GET", "POST"],
     },
   });
